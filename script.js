f2cdadaa6de59f1a7f54226ab6f821148c72ac25
targolghasemi <targolghasemi85@gmail.com>
2025-09-27 16:04:43 +0330

    clicking edited

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-function () {
+function initCalculator() {
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
 
-    //وقتی روی اعداد و عملگرها کلید شد در بالا نمایش بده
-
+    // وقتی روی اعداد و عملگرها کلیک بشه
     buttons.forEach(function (button) {
-        button.addEventListener('click',function (e) {
+        button.addEventListener('click', function (e) {
             let value = e.target.dataset.num;
             screen.value += value;
-        })
+        });
     });
-} 
+}
+
+initCalculator();
