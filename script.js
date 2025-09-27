423fe55076579df5b0361c7449ebda97e795e2c9
targolghasemi <targolghasemi85@gmail.com>
2025-09-27 16:00:24 +0330

    clicking on the buttons

---
+++ script.js
@@ -0,0 +1,15 @@
+function () {
+    let screen = document.querySelector('.screen');
+    let buttons = document.querySelectorAll('.btn');
+    let clear = document.querySelector('.btn-clear');
+    let equal = document.querySelector('.btn-equal');
+
+    //وقتی روی اعداد و عملگرها کلید شد در بالا نمایش بده
+
+    buttons.forEach(function (button) {
+        button.addEventListener('click',function (e) {
+            let value = e.target.dataset.num;
+            screen.value += value;
+        })
+    });
+} 
