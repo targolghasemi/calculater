365e6d0ac12dccf3121b6b80024a962c1b9cf879
targolghasemi <targolghasemi85@gmail.com>
2025-09-27 16:20:43 +0330

    clear and equal buttons

--- script.js
+++ script.js
@@ -11,6 +11,22 @@ function initCalculator() {
             screen.value += value;
         });
     });
+
+    //وقتی رو مساوی کلیک بشه 
+    equal.addEventListener('click', function (e) {
+        if (screen.value === '') {
+            screen.value = 'Please Enter a Value';
+        }else{
+            let answer = eval(screen.value);
+            screen.value = answer;
+
+        }
+    })
+
+    //وقتی رو کلیر کلیک بشه
+    clear.addEventListener('click' , function (e) {
+        screen.value = '';
+    })
 }
 
 initCalculator();
